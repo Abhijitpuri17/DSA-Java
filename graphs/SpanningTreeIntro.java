76e4fc81bfe3055483f76ae3e9d0aa26e1ec420d
Abhijit Puri <puriabhijit000@gmail.com>
2021-10-01 18:52:07 +0530

    Prim's Algorithm

---
+++ graphs/SpanningTreeIntro.java
@@ -0,0 +1,21 @@
+package graphs;
+
+public class SpanningTreeIntro
+{
+
+	/*
+	  
+	   **For a connected graph, sub-graph of the graph such that no. of edges = no. of vertices - 1
+	      no cycle is formed
+	  
+	   ** If a graph has E edges and v vertices, no of spanning trees = ⁿCv-1 - cycles
+	  
+	   ** COST of SPANNING Tree = sum of costs of each edge of spanning tree
+	   
+	   ** To find minimum cost spanning tree , there are algorithms - Prim's and Kruskal's algorithm
+	  
+	  
+	 */
+	
+	
+}
