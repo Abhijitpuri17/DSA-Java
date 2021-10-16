f62cd0fc9b5a106ee0197e9d05d581847aa2c85c
Abhijit Puri <85777554+Abhijitpuri17@users.noreply.github.com>
2021-10-16 08:21:30 +0530

    LongestSubstringNoDuplicates: Added comments

--- slidingWindow/LongestSubstringNoDuplicates.java
+++ slidingWindow/LongestSubstringNoDuplicates.java
@@ -8,20 +8,24 @@ public class LongestSubstringNoDuplicates
 	static String unique_substring(String s)
 	{
 		
+		// elements are stored in same order we add them in linkedHashSet
 		LinkedHashSet<Character> set = new LinkedHashSet<>();
 		
 		int i = 0 , j = 0 , start = 0, end = 0;
+		
 		
 		while (j < s.length())
 		{
 			char c = s.charAt(j) ;
 			
+			// if c is already present in window. Start contracting the window from left till c is not removed.
 			while (set.contains(c))
 			{
 				set.remove(set.iterator().next()) ;
 				i++ ;
 			}
 			
+			// now add c at last and check if this substring can be longest or not
 			set.add(c) ;
 			if (j - i > end - start) {
 				start = i ;
