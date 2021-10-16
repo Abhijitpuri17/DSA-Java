0141b11743029c5b0a84268c31b7f1168dded6ae
Abhijit Puri <85777554+Abhijitpuri17@users.noreply.github.com>
2021-10-16 08:34:55 +0530

    Prim's Algorithm : Added comments

--- greedyAlgorithm/PrimsAlgorithm.java
+++ greedyAlgorithm/PrimsAlgorithm.java
@@ -2,6 +2,11 @@ package graphs;
 
 public class PrimsAlgorithm 
 {
+	
+	// First selected minimum cost edge. Now mainting the connection with selected nodes, keep finding min edges
+	
+	
+	
 	static final long I = Long.MAX_VALUE;
 	
 	static long costs[][] = { 
@@ -15,7 +20,9 @@ public class PrimsAlgorithm
 			{I, I, 10, I, 24, 18, I, I}
 			} ;
 	
+	// near array denotes min cost of edge connecting ith vertex with previously selected vertics
 	static long near[] = {I, I, I, I, I, I, I, I};
+	
 	static long ans[][] = new long[2][6] ;
 	
 	
@@ -25,6 +32,7 @@ public class PrimsAlgorithm
 		int u = 0, v = 0, n = 8;
 		long min = Long.MAX_VALUE;
 		
+		// fist find the minimum cost edge and add that edge to answer
 		for (int i = 0 ; i < n ; i++)
 		{
 			for (int j = i ; j < n ; j++)
@@ -39,6 +47,7 @@ public class PrimsAlgorithm
 		ans[0][0] = u ; ans[1][0] = v ;
 		near[u] = near[v] = 0 ;
 		
+		// For each vertex check which edge(with u or with v) has min cost
 		for (int i = 1 ; i < n ; i++)
 		{
 			if (near[i] == 0) continue;
@@ -49,9 +58,12 @@ public class PrimsAlgorithm
 		
 		int k = 0;
 		
+		// now repeat the process for remaining edges of spanning tree
 		for (int i = 1 ; i < n-2 ; i++)
 		{
 			long min_curr = I ;
+			
+			// find min cost edge (but should have connection with previously selected edge)
 			for (int j = 1 ; j < n ; j++)
 			{
 				if (near[j] != 0 && costs[j][(int) near[j]] < min_curr)
@@ -61,10 +73,11 @@ public class PrimsAlgorithm
 				}
 			}
 			
+			
 			ans[0][i] = k ; ans[1][i] = near[k] ;
 			near[k] = 0 ;
 			
-			
+			// update near array... i.e. for each vertex which edge has min cost (with previously selected only)
 			for (int j = 1 ; j < n ; j++)
 			{
 				if (near[j] != 0 && costs[j][k] < costs[j][(int) near[j]])
